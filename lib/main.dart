efd89aa040c2b20ac17ac3062dfa8d5f18633b8d
Yoshi <yoskomu.33@gmail.com>
2021-11-10 23:19:30 +0900

    Firebaseログイン機能の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:html';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,26 +19,93 @@ class ChatApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String infoText = '';
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('ログイン'),
-              onPressed: () async {
-                await Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) {
-                    return ChatPage();
-                  }),
-                );
-              },
-            )
-          ],
-        ),
+        child: Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: "メールアドレス"),
+                  onChanged: (String value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                    decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                    onChanged: (String value) {
+                      setState(() {
+                        password = value;
+                      });
+                    }),
+                const SizedBox(height: 8),
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final FirebaseAuth auth = FirebaseAuth.instance;
+                        final UserCredential result =
+                            await auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+
+                        final User user = result.user!;
+                        setState(() {
+                          infoText = "登録OK:${user.email}";
+                        });
+                      } catch (e) {
+                        setState(() {
+                          infoText = "登録NG:${e.toString()}";
+                        });
+                      }
+                    },
+                    child: Text("ユーザー登録"),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () async {
+                      try {
+                        final FirebaseAuth auth = FirebaseAuth.instance;
+                        await auth.signInWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        await Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) {
+                            return ChatPage();
+                          }),
+                        );
+                      } catch (e) {
+                        setState(() {
+                          infoText = "ログインNG:${e.toString()}";
+                        });
+                      }
+                    },
+                    child: Text("ログイン"),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(infoText)
+              ],
+            )),
       ),
     );
   }
