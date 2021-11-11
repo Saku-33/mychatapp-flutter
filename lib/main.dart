3669a2486670a346017b5ef6ee42c31c74e887c9
Yoshikawa Misa <misyoshika@gmail.com>
2021-11-11 23:09:24 +0900

    Firestoreへデータ保存

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -141,7 +142,7 @@ class ChatPage extends StatelessWidget {
         onPressed: () async {
           await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              return AddPostPage();
+              return AddPostPage(user);
             }),
           );
         },
@@ -150,21 +151,57 @@ class ChatPage extends StatelessWidget {
   }
 }
 
-class AddPostPage extends StatelessWidget {
+class AddPostPage extends StatefulWidget {
+  AddPostPage(this.user);
+  final User user;
+
+  @override
+  _AddPostPageState createState() => _AddPostPageState();
+}
+
+class _AddPostPageState extends State<AddPostPage> {
+  String messageText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("チャット投稿"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("戻る"),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: '投稿メッセージ'),
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              onChanged: (String value) {
+                setState(() {
+                  messageText = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: Text('投稿'),
+                onPressed: () async {
+                  final date = DateTime.now().toLocal().toIso8601String();
+                  final email = widget.user.email;
+                  await FirebaseFirestore.instance
+                      .collection('post')
+                      .doc()
+                      .set({
+                    'text': messageText,
+                    'email': email,
+                    'date': date,
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+            )
+          ]),
     );
   }
 }
